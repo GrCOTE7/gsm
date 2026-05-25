879a2492eb15b3bfbd8e4441e83a89e0993fe0b4
GrCOTE7 <grcote7@gmail.com>
2026-05-25 14:48:33 +0200

    nett

--- src/upu/views/tests.py
+++ src/upu/views/tests.py
@@ -69,7 +69,7 @@ def build() -> ft.Control:
             ],
             alignment=ft.MainAxisAlignment.CENTER,
         ),
-        "Page de MP21170 pour tests rapides.",
+        "Page pour tests rapides.",
         extra_top_gap=0,
         extra=ft.Column(
             [
