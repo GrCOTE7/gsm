2b3faf97e415f435b0c136c0b86f9a0bda9ff0e7
GrCOTE7 <GrCOTE7@gmail.com>
2026-06-03 16:52:32 +0200

    Nett

--- src/upu/views/tests.py
+++ src/upu/views/tests.py
@@ -9,30 +9,21 @@ def ready():
     return ft.Text("→ Ready for quick test21!", size=14, weight=ft.FontWeight.W_400)
 
 
-def build() -> ft.Control:
-
-    return named_view(
-        ft.Row(
-            controls=[
-                ft.Icon(ft.Icons.SCIENCE, size=28),
-                ft.Text("Tests", size=28, weight=ft.FontWeight.W_600),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        "Page pour tests rapides.",
-        extra_top_gap=0,
-        extra=tests_views(),
+def _tests_header() -> ft.Row:
+    return ft.Row(
+        controls=[
+            ft.Icon(ft.Icons.SCIENCE, size=28),
+            ft.Text("Tests", size=28, weight=ft.FontWeight.W_600),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
     )
 
 
-def main(page: ft.Page):
-    page.add(
-        ft.Row(
-            controls=[
-                ft.Icon(ft.Icons.SCIENCE, size=28),
-                ft.Text("Tests", size=28, weight=ft.FontWeight.W_600),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        ),
-        # tests_views(), # 
+def build() -> ft.Control:
+
+    return named_view(
+        _tests_header(),
+        "Page pour tests rapides.",
+        extra_top_gap=0,
+        extra=tests_views(),
     )
