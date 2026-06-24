6132f854aed3eabaf5c86538c921222508ba23f6
MP21170 <raquin.mariepierre@gmail.com>
2026-06-24 15:59:01 +0000

    automatic start

--- quick_test.py
+++ quick_test.py
@@ -1,22 +1,8 @@
-# from scripts.essays.salutation import cls, hi
-
-# Script juste pour tester le HotReload en CodeSpace
+from scripts.essays.salutation import show
 
 # uvx watchfiles "python quick_test.py" .
 
-CHAINE = "Look bien la CLI, et change un caractère dans la variable CHAINE !"
+if __name__ == "__main__":
 
-def hi():
-    return CHAINE
-
-def cls():
-    return "\033c"
-
-# while True:
-print(
-    cls() +
-    "\n".join(
-        [" ".join(str(i*j) for j in range(1, i+1)) for i in range(1, 4)]
-        + ["", "Ready pour la suite.", "", hi()]
-    )
-)
+    show()
+    print('\nFin de ./essays/salutation.py.')
--- scripts/essays/salutation.py
+++ scripts/essays/salutation.py
@@ -1,12 +1,31 @@
 # Script juste pour tester le HotReload en CodeSpace
 
-# 1fo Le message affiché est défini dans MESSAGE_TEST
-# 1fo Change un caractère dans MESSAGE_TEST pour tester le reload !
+# 1fo Change un caractère dans chaine pour tester le reload !
+# 1fo  1. Le message affiché dans la console est défini dans la variable chaine
 
-MESSAGE_TEST = "Look bien la CLI, et change un caractère dans la variable chaîne !"
+chaine = "Look bien la CLI, et change un caractère (ou +) dans cette chaine !"
+nombre_de_lignes = 3 # 2do ← 2. Change ici aussi !
 
-def hi():
-    return MESSAGE_TEST
+# 1fo  3. Ferme le hotreload avec CTRL + MAJ + C
+# 2see 4. Essaie ./go dans le CLI ...
 
 def cls():
     return "\033c"
+
+def hi():
+    return chaine
+
+def show():
+    
+    print(
+    cls() +
+    "\n".join(
+        [" ".join(str(i*j) for j in range(1, i+1)) for i in range(1, nombre_de_lignes+1)]
+        + ["", "Ready pour la suite.", "", hi()]
+        )
+    )
+
+
+if __name__ == "__main__":
+
+    show()
