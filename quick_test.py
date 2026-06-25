20db49f1b43f627708f55ef2cd4ccf06f9b32b9b
GrCOTE7 <GrCOTE7@gmail.com>
2026-06-25 21:40:40 +0200

    up code

--- quick_test.py
+++ quick_test.py
@@ -1,40 +1,24 @@
-from scripts.essays.salutation import cls, show
+from scripts.essays.salutation import cls, disclaimer, show
 from rich import print
 
-import time
-import sys
+
 # uvx watchfiles "python quick_test.py" .
 
-def disclamer():
-
-    texte = "Attention, mesdames et messieurs, dans un instant, ça va commencer... Heu, non ! Vous avez déjà la main !\n"
-
-    print(
-        cls(False) + 
-        "\n[bold red]Disclaimer[/bold red] :\n"
-        "Ce script est un exemple de test pour le hotreload en CodeSpace.\n"
-        "Il n'a pas de fonctionnalité particulière et peut être modifié à volonté.\n"
-        "Il est surtout recommandé de ne pas l'utiliser en production.\n"
-    )
-
-    for lettre in texte:
-        print(lettre, end="", flush=True)
-        time.sleep(0.05)
-    time.sleep(1)
-    # cls()
 
 if __name__ == "__main__":
 
     cls()
 
-    disclamer()
+    disclaimer()
 
     show()
 
-    print(f'\nFin de ./quick_test.py :Ambassadorship:')
+    print(f"\nFin de ./quick_test.py :Ambassadorship:")
 
-    print('\n( [italic]Dans la CLI, [bold blue]CTRL + Clic[/bold blue] pour ouvrir un fichier écrit en magenta.[/italic] :wink: )')
-    
+    print(
+        "\n( [italic]Dans la CLI, [bold blue]CTRL + Clic[/bold blue] pour ouvrir un fichier écrit en magenta.[/italic] :wink: )"
+    )
+
     # print(locals()) # ← CTRL + / pour switch commentaire
 
-# python.execInTerminal-icon     
+# python.execInTerminal-icon
--- scripts/essays/salutation.py
+++ scripts/essays/salutation.py
@@ -1,41 +1,67 @@
 from rich import print
 
+import time
+import sys
+
 # Script juste pour tester le HotReload en CodeSpace
 
 # 1fo Change un caractère dans chaine pour tester le reload !
 # 1fo  1. Le message affiché dans la console est défini dans la variable chaine
 
 chaine = "Look bien la CLI, et change un caractère (ou +) dans la variable chaine\n(File ./scripts/essays/salutation.py)!"
-nombre_de_lignes = 3 # 2do ← 2. Change ici aussi !zzzz
+nombre_de_lignes = 3  # 2do ← 2. Change ici aussi !zzzz
 
 # 1fo  3. Ferme le hotreload avec CTRL + MAJ + C
 # 2see 4. Essaie ./go dans le CLI ...aa
 # 2see 5. Teste sur un script actif, les modes: F9 : RUN - F5 : Debug - F8 : hotreload
 
+
 def cls(mode: bool = True) -> str:
-    
+
     code = "\033c"
 
     # 2ar
-    code = "" 
+    code = ""
 
     if mode:
         print(code, end="")
-        return ''
+        return ""
 
     return code
+
+
+def disclaimer():
+
+    texte = "Attention, mesdames et messieurs, dans un instant, ça va commencer... Heu, non ! Vous avez déjà la main !\n"
+
+    print(
+        cls(False) + "\n[bold red]Disclaimer[/bold red] :\n"
+        "Ce script est un exemple de test pour le hotreload en CodeSpace.\n"
+        "Il n'a pas de fonctionnalité particulière et peut être modifié à volonté.\n"
+        "Il est surtout recommandé de ne pas l'utiliser en production.\n"
+    )
+
+    for lettre in texte:
+        print(lettre, end="", flush=True)
+        time.sleep(0.05)
+    time.sleep(1)
+    # cls()
 
 
 def hi():
     return chaine
 
+
 def show():
-    
+
     print(
-    cls() +
-    "\n".join(
-        [" ".join(str(i*j) for j in range(1, i+1)) for i in range(1, nombre_de_lignes+1)]
-        + ["", "Ready pour la suite ?", "", hi()]
+        cls()
+        + "\n".join(
+            [
+                " ".join(str(i * j) for j in range(1, i + 1))
+                for i in range(1, nombre_de_lignes + 1)
+            ]
+            + ["", "Ready pour la suite ?", "", hi()]
         )
     )
 
