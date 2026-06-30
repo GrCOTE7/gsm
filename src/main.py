a5bc213a5acd27784702da49829b8476452e21bc
GrCOTE7 <GrCOTE7@gmail.com>
2026-06-30 17:39:33 +0200

    2fix docker

--- src/main.py
+++ src/main.py
@@ -1,26 +1,27 @@
 import os
 import flet as ft
-import flet_audio  # noqa: F401
 
 from upu.controllers.app_controller import create_app
 
-# Détection automatique : Docker ou local ?
 RUNNING_IN_DOCKER = os.environ.get("FLET_DOCKER", "0") == "1"
 
 if RUNNING_IN_DOCKER:
-    # Mode Web pour Docker
-    ft.app(
+    ft.run(
         target=create_app,
         host="0.0.0.0",
-        port=8000
-    )
+        port=8000,
+        view=ft.View.WEB, #type: ignore
+    ) #type: ignore
 else:
-    # Mode Desktop pour ton PC
     ft.run(create_app)
+
+
 
 # Docker desktop doit être lancé
 # docker compose up -d --build
 # OU
+
 # docker compose build --no-cache
+# docker compose up -d
 
 # docker exec -it gsm_app sh  
