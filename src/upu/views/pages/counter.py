a1ca2520998da3f39a39fcd6f494520a17b2ff7c
GrCOTE7 <GrCOTE7@gmail.com>
2026-07-23 18:47:14 +0200

    try declarative mode

--- src/upu/views/pages/counter.py
+++ src/upu/views/pages/counter.py
@@ -1,20 +1,45 @@
 import flet as ft
 
 
+def imperative_counter() -> ft.Control:
+    count = 0
+    text = ft.Text("0")
+
+    def increment(_e) -> None:
+        nonlocal count
+        count += 1
+        text.value = str(count)
+
+    return ft.Row(
+        [
+            text,
+            ft.Button("Increment", on_click=increment),
+        ]
+    )
+
+
+# type: ignore
+
+
+@ft.component
+def declarative_counter_component() -> ft.Control:
+    count, set_count = ft.use_state(0)
+
+    return ft.Row(
+        [
+            ft.Text(str(count)),
+            ft.Button(
+                "Increment",
+                on_click=lambda _: set_count(count + 1),
+            ),
+        ]
+    )
+
+
 def build() -> ft.Control:
-    counter_value = ft.Text("0")
-
-    def increment(_e: ft.ControlEvent) -> None:
-        counter_value.value = str(int(counter_value.value) + 1)
-
-    return ft.Container(
-        padding=20,
-        content=ft.Column(
-            spacing=12,
-            controls=[
-                ft.Text("Compteur", size=24, weight=ft.FontWeight.W_600),
-                counter_value,
-                ft.Button("Increment", key="increment", on_click=increment),
-            ],
-        ),
+    return ft.Column(
+        [
+            imperative_counter(),
+            # declarative_counter_component(),
+        ]
     )
