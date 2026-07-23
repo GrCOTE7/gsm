06cba5698b2b5cf0556dab703fe8b5dad2b02a4f
GrCOTE7 <GrCOTE7@gmail.com>
2026-07-23 19:01:24 +0200

    reset imperative mode counter

--- src/upu/views/pages/counter.py
+++ src/upu/views/pages/counter.py
@@ -1,45 +1,48 @@
 import flet as ft
 
+from gc7_tools.helpers import sepa_major, sepa_outlined
+
 
 def imperative_counter() -> ft.Control:
-    count = 0
-    text = ft.Text("0")
+    count = ft.Text("0")
 
-    def increment(_e) -> None:
-        nonlocal count
-        count += 1
-        text.value = str(count)
+    def increment(_e: ft.ControlEvent) -> None:
+        count.value = str(int(count.value) + 1)
 
-    return ft.Row(
-        [
-            text,
-            ft.Button("Increment", on_click=increment),
-        ]
-    )
+# * [ ] ici transitional btn
 
-
-# type: ignore
-
-
-@ft.component
-def declarative_counter_component() -> ft.Control:
-    count, set_count = ft.use_state(0)
-
-    return ft.Row(
-        [
-            ft.Text(str(count)),
-            ft.Button(
-                "Increment",
-                on_click=lambda _: set_count(count + 1),
-            ),
-        ]
+    return ft.Container(
+        padding=20,
+        content=ft.Row(
+            spacing=12,
+            controls=[
+                ft.Text(
+                    "Imperative mode",
+                    size=24,
+                    weight=ft.FontWeight.W_600,
+                ),
+                count,
+                ft.Button(
+                    "Increment",
+                    key="increment",
+                    on_click=increment,  # type: ignore
+                ),
+            ],
+        ),
     )
 
 
 def build() -> ft.Control:
-    return ft.Column(
-        [
-            imperative_counter(),
-            # declarative_counter_component(),
-        ]
+
+    return ft.Container(
+        padding=20,
+        content=ft.Column(
+            spacing=12,
+            controls=[
+                ft.Text("Compteurs", size=24, weight=ft.FontWeight.W_600),
+                sepa_outlined(),
+                imperative_counter(),
+                sepa_outlined(),
+            ],
+        ),
     )
